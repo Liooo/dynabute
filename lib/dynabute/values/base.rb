7b71ed2d970d5019039cae5de09c38b0d080bf57
Liooo <ryoyamada3@gmail.com>
2017-08-26 22:01:56 +0900

    mod: value.dynabute_field -> .field

--- lib/dynabute/values/base.rb
+++ lib/dynabute/values/base.rb
@@ -7,10 +7,10 @@ module Dynabute::Values
     included do
       def self.table_name_prefix; Dynabute::Util.table_name_prefix; end
       belongs_to :dynabutable, polymorphic: true
-      belongs_to :dynabute_field, class_name: 'Dynabute::Field'
+      belongs_to :field, class_name: 'Dynabute::Field'
 
       def value_type
-        dynabute_field.value_type
+        field.value_type
       end
     end
 
