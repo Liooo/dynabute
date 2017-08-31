0795d23e4e60df75cb88fa01aba93766ef406db1
Liooo <ryoyamada3@gmail.com>
2017-08-31 23:51:30 +0900

    fix: fixed datetime value type
    omfg

--- lib/generators/dynabute/templates/migration.rb
+++ lib/generators/dynabute/templates/migration.rb
@@ -39,7 +39,7 @@ class CreateDynabutes < ActiveRecord::Migration[5.1]
       t.integer  "field_id", limit: 4
       t.integer  "dynabutable_id", limit: 4
       t.string   "dynabutable_type", limit: 50
-      t.boolean  "value"
+      t.datetime "value"
     end
     add_index "dynabute_datetime_values", ["dynabutable_id"], name: "dynabute_datetime_values_on_recordable_id", using: :btree
     add_index "dynabute_datetime_values", ["dynabutable_id", "field_id"], name: "dynabute_datetime_values_on_record_id_and_recordable_id", using: :btree
